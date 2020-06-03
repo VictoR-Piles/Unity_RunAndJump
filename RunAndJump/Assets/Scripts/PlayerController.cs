b7463e0d5227372b18155b42cbf555f884a3c78a
VictoR-Piles <victorpilesdelgado@gmail.com>
2020-06-03 19:36:33 +0200

    Movimiento del Player y el BG terminados

---
+++ RunAndJump/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float gravityModifier = 1f;
+	public float jumpForce = 10f;
+
+	private Rigidbody rb;
+	private bool onGround = true;
+
+	void Start()
+	{
+		Physics.gravity *= gravityModifier;
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space) && onGround)
+		{
+			rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+			onGround = false;
+		}
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		onGround = true;
+	}
+}
---
+++ RunAndJump/Assets/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+	public GameObject obstacelPrefab;
+	public float startDelay = 2f;
+	public float spawnInterval = 2f;
+
+	private Vector3 spawnPos = new Vector3(30, 0, 0);
+
+	void Start()
+	{
+		InvokeRepeating("SpawnObstacle", startDelay, spawnInterval);
+	}
+
+	void Update()
+	{
+	}
+
+	void SpawnObstacle()
+	{
+		Instantiate(obstacelPrefab, spawnPos, obstacelPrefab.transform.rotation);
+	}
+}
